e9a2126e3757c671ffbd44690df8b328b823a9f9
Vitor Serigati <vitor_serigati@hotmail.com>
2023-11-19 21:41:57 -0300

    feat(sinmple connection): created a simple connection and server echo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello World!")
+    let listener: TcpListener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+    let (read, mut writer) = socket.split();
+    let mut reader = BufReader::new(read);
+    let mut line = String::new();
+
+    loop {
+        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        if bytes_read == 0 {
+            break;
+        }
+
+        writer.write_all(&line.as_bytes()).await.unwrap();
+        line.clear();
+    }
 }
